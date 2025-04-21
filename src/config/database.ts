03de29ff02aa6a89e78e553558aedf89ef77ca86
rubencito9294 <74841337+rubencito9294@users.noreply.github.com>
2025-04-21 02:59:46 -0500

    fix mongouri

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,31 +5,32 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const connectDB = async (): Promise<void> => {
-    try {
-        const user = process.env.USER;
-        const password = process.env.PASSWORD;
-        const dbName = process.env.DB_NAME;
+  try {
+    const user = process.env.USER;
+    const password = process.env.PASSWORD;
+    const dbName = process.env.DB_NAME;
+    const mongoURI = process.env.MONGO_URI || "";
 
-        const mongoURI = `mongodb://${user}:${password}@127.0.0.1:27017/${dbName}?authSource=admin`;
-        console.log("Ruben: mongoURI", mongoURI);
+    // const mongoURI =  `mongodb://${user}:${password}@127.0.0.1:27017/${dbName}?authSource=admin`;
+    console.log("Ruben: mongoURI", mongoURI);
 
-        await mongoose.connect(mongoURI);
-        console.log("MongoDB conectado");
+    await mongoose.connect(mongoURI);
+    console.log("MongoDB conectado");
 
-        // const db = mongoose.connection.db;
-        const admin = mongoose.connection?.db?.admin();
-        const dbList = await admin?.listDatabases();
-        const dbExists = dbList?.databases.some((db) => db.name === dbName);
+    // const db = mongoose.connection.db;
+    const admin = mongoose.connection?.db?.admin();
+    const dbList = await admin?.listDatabases();
+    const dbExists = dbList?.databases.some((db) => db.name === dbName);
 
-        if (!dbExists) {
-            console.log(`Base de datos '${dbName}' creada.`);
-        } else {
-            console.log(`Base de datos '${dbName}' ya existe.`);
-        }
-    } catch (error) {
-        console.error("Error al conectar a MongoDB:", error);
-        process.exit(1);
+    if (!dbExists) {
+      console.log(`Base de datos '${dbName}' creada.`);
+    } else {
+      console.log(`Base de datos '${dbName}' ya existe.`);
     }
+  } catch (error) {
+    console.error("Error al conectar a MongoDB:", error);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
